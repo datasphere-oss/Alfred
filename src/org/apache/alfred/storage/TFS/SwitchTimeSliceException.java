0476ed2a490a31ec1f1d356b0fcaaea86073eda7
theseus yang <theseusyang@users.noreply.github.com>
2022-02-25 15:24:54 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/SwitchTimeSliceException.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.tfs;
+
+/**
+ * Thrown when a writer attempts to insert into a block, but there is no room.
+ * By the time this exception is thrown, the previous time slice has been
+ * checked in (for the accessor), and a new time slice, contained in this
+ * exception, has been checked out to the accessor.
+ */
+public class SwitchTimeSliceException extends RuntimeException {
+    public final TimeSlice newTimeSlice;
+
+    public SwitchTimeSliceException(TimeSlice newTimeSlice) {
+        this.newTimeSlice = newTimeSlice;
+    }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        return (null);
+    }
+}
