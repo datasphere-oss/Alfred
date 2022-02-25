5229f47f2cb3172c98960c1af0a2248af3c75b9c
theseus yang <theseusyang@users.noreply.github.com>
2022-02-25 14:40:06 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/BufferedStreamUtil.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.tfs;
+
+import org.apache.alfred.fs.chunkcache.ChunkCacheInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+/**
+ * @author Alexei Osipov
+ */
+class BufferedStreamUtil {
+    private BufferedStreamUtil() {
+    }
+
+    /**
+     * Wraps stream into a {@link BufferedInputStream} if this stream is not already
+     * buffered.
+     * <p>
+     *
+     * This method helps to avoid double buffering of data.
+     */
+    static InputStream wrapWithBuffered(InputStream in) {
+        if (in instanceof BufferedInputStream || in instanceof ChunkCacheInputStream) {
+            // This is already a buffered stream. Don't wrap it.
+            return in;
+        } else {
+            // Buffer data from this stream
+            return new BufferedInputStream(in);
+        }
+    }
+}
