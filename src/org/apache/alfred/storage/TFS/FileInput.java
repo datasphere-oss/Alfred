abd54b6ed536e8cf056d19ea95cc71866083ac70
theseus yang <theseusyang@users.noreply.github.com>
2022-02-25 15:06:43 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/FileInput.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.tfs;
+
+import org.apache.alfred.storage.tfs.pub.AbstractPath;
+import org.apache.alfred.storage.tfs.codecs.BlockDecompressor;
+import org.apache.alfred.storage.tfs.DataBlock;
+import org.apache.alfred.util.collections.ByteArray;
+import org.apache.alfred.util.io.IOUtil;
+import org.apache.alfred.util.lang.Util;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+class FileInput {
+    private long offsetInFile = -1;
+
+    private InputStream is;
+    private InputStream bis;
+
+    private boolean opened = false;
+    private ByteArray buffer;
+
+    void open(AbstractPath path, int offset) throws IOException {
+        assert !opened;
+
+        is = path.openInput(offset);
+        bis = BufferedStreamUtil.wrapWithBuffered(is);
+
+        opened = true;
+        offsetInFile = offset;
+    }
+
+    public void read(DataBlock into, int length, BlockDecompressor decompressor) throws IOException {
+        ByteArray data = into.getData();
+
+        if (decompressor == null) {
+            IOUtil.readFully(bis, data.getArray(), data.getOffset(), into.getDataLength());
+        } else if (length > 0) {
+            if (buffer == null)
+                buffer = new ByteArray(length);
+
+            if (buffer.getLength() < length)
+                buffer.setArray(new byte[length], 0, length); // @ALLOCATION
+
+            int size = IOUtil.readFully(bis, buffer.getArray(), buffer.getOffset(), length);
+            assert size == 0 : ("readFully (" + length + ") != 0");
+
+            decompressor.inflate(buffer.getArray(), buffer.getOffset(), length, data.getArray(), data.getOffset(),
+                    into.getDataLength());
+        }
+
+        offsetInFile += length;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+    void seek(long offset) throws IOException {
+        long skip = offset - offsetInFile;
+
+        if (skip < 0)
+            throw new IllegalStateException("seek back from " + offsetInFile + " to " + offset);
+
+        if (skip > 0) {
+            IOUtil.skipFully(bis, skip);
+            offsetInFile = offset;
+        }
+    }
+
+    boolean close() {
+        // inputs reused in pool - we have to clear all
+        offsetInFile = -1;
+
+        if (is != null) {
+            Util.close(is);
+            bis = null;
+            is = null;
+        }
+
+        if (opened) {
+            opened = false;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void setOffset(int offset) {
+        this.offsetInFile = offset;
+    }
+
+    public long getOffsetInFile() {
+        return offsetInFile;
+    }
+
+    /**
+     * @return wrapped (buffered) input stream
+     */
+    public InputStream getInputStream() {
+        return bis;
+    }
+}
