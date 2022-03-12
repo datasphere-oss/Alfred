6ba5b1e33102cdaaf55585b7e1a23580299c4eb1
theseus yang <theseusyang@users.noreply.github.com>
2022-03-12 18:17:21 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/pub/SingleEntityFilter.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.pub;
+
+/**
+ *
+ */
+public class SingleEntityFilter implements AbstractSingleEntityFilter {
+    public int entity;
+
+    public SingleEntityFilter(int entity) {
+        this.entity = entity;
+    }
+
+    @Override
+    public boolean accept(int entity) {
+        return (this.entity == entity);
+    }
+
+    @Override
+    public boolean acceptAll() {
+        return (false);
+    }
+
+    @Override
+    public int getSingleEntity() {
+        return (entity);
+    }
+
+    @Override
+    public String toString() {
+        return "SingleEntityFilter [" + entity + "]";
+    }
+
+    @Override
+    public boolean restrictAll() {
+        return false;
+    }
+
+    @Override
+    public long acceptFrom(int entity) {
+        return Long.MIN_VALUE;
+    }
+}
