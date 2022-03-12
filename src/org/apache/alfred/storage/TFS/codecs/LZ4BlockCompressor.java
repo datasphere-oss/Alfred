2688aa4f27e084803947456b15f9dca800c68f07
theseus yang <theseusyang@users.noreply.github.com>
2022-03-12 17:42:07 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/codecs/LZ4BlockCompressor.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.codecs;
+
+import org.apache.alfred.util.collections.generated.ByteArrayList;
+import net.jpountz.lz4.LZ4Compressor;
+import net.jpountz.lz4.LZ4Factory;
+
+/**
+ *
+ */
+public class LZ4BlockCompressor extends BlockCompressor {
+    private final LZ4Compressor compressor;
+
+    public LZ4BlockCompressor(int compressionLevel, ByteArrayList buffer) {
+        super(buffer);
+
+        if (compressionLevel == 1)
+            compressor = LZ4Factory.fastestInstance().fastCompressor();
+        else
+            compressor = LZ4Factory.fastestInstance().highCompressor(compressionLevel - 1);
+    }
+
+    @Override
+    public byte code() {
+        return BlockCompressorFactory.getCode(Algorithm.LZ4);
+    }
+
+    public int deflate(byte[] src, int offset, int length, ByteArrayList appendTo) {
+        int maxCompressedLength = compressor.maxCompressedLength(length);
+
+        int size = appendTo.size();
+        appendTo.ensureCapacity(size + maxCompressedLength);
+        byte[] data = appendTo.getInternalBuffer();
+        int compressedLength = compressor.compress(src, offset, length, data, size, maxCompressedLength);
+        appendTo.setSize(size + compressedLength);
+
+        return compressedLength;
+    }
+}
