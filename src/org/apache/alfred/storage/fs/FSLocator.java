1b1620824279636302f6a8237d4a410b8136227f
theseus yang <theseusyang@users.noreply.github.com>
2022-03-12 22:09:42 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/fs/FSLocator.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.fs;
+
+import org.apache.alfred.gflog.api.*;
+import org.apache.alfred.storage.fs.pub.AbstractFileSystem;
+import org.apache.alfred.storage.fs.pub.AbstractPath;
+import org.apache.alfred.storage.fs.pub.FSFactory;
+
+import java.io.IOException;
+
+public class FSLocator {
+    private static final Log LOGGER = LogFactory.getLog(FSLocator.class);
+
+    private AbstractPath root;
+    private final String rootPath;
+
+    public FSLocator(String rootPath) {
+        this.rootPath = rootPath;
+
+        try {
+            createRootPathIfEmpty();
+        } catch (Exception e) {
+            LOGGER.log(LogLevel.WARN).append("Error initializing FileSystem locator").append(e).commit();
+        }
+    }
+
+    private void createRootPathIfEmpty() throws IOException {
+        if (root == null) {
+            if (rootPath == null || rootPath.isEmpty())
+                throw new IllegalStateException("FileSystem location root is empty");
+
+            root = FSFactory.createPath(rootPath);
+        }
+    }
+
+    public synchronized String getPath(String name) {
+        try {
+            createRootPathIfEmpty();
+            return root.getFileSystem().createPath(root, name).getPathString(); // add absolute path function
+        } catch (IOException ioe) {
+            throw new com.epam.deltix.util.io.UncheckedIOException(ioe);
+        }
+    }
+
+    public synchronized String getPath(String name, String subPath) {
+        try {
+            createRootPathIfEmpty();
+            AbstractFileSystem fs = root.getFileSystem();
+            return fs.createPath(fs.createPath(root, name), subPath).getPathString(); // add absolute path function
+        } catch (IOException ioe) {
+            throw new com.epam.deltix.util.io.UncheckedIOException(ioe);
+        }
+    }
+}
