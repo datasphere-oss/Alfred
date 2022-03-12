fc9ec4cbd2a601b4bfaa0a9de24c7a2d5c2d198f
theseus yang <theseusyang@users.noreply.github.com>
2022-03-12 18:19:10 +0800

    Add files via upload

---
+++ src/org/apache/alfred/storage/TFS/pub/TSMessageConsumer.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.pub;
+
+import org.apache.alfred.messages.service.RealTimeStartMessage;
+import org.apache.alfred.util.memory.MemoryDataInput;
+
+/**
+ * Processes a message.
+ */
+public interface TSMessageConsumer {
+    /**
+     * Process next message.
+     * 
+     * @param entity
+     * @param timestampNanos Message timestamp. All times are in nanoseconds.
+     * @param type
+     * @param bodyLength
+     * @param mdi
+     */
+    void process(int entity, long timestampNanos, int type, int bodyLength, MemoryDataInput mdi);
+
+    /**
+     * Notifies consumer about the end of historical data portion
+     *
+     * @return true if consumer supports real time mode notification and will emit
+     *         RealTimeStartMessage as the next message from this source.
+     */
+    boolean processRealTime(long timestampNanos);
+
+    /**
+     * @return true if this source already switched from historical to real-time
+     *         data portion (Only when {@link #realTimeAvailable()}).
+     */
+    boolean isRealTime();
+
+    /**
+     * @return true if source can be switched in real-time.
+     *
+     *         When realtime mode is available client can use method
+     *         {@link #isRealTime()} ()} to detect switch from historical to
+     *         real-time portion of data. Also in this mode client will receive
+     *         special message {@link RealTimeStartMessage} .
+     */
+    boolean realTimeAvailable();
+}
---
+++ src/org/apache/alfred/storage/TFS/pub/TSRoot.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ *
+ *
+ *  Copyright (c) 2022 Alfred
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.apache.alfred.storage.pub;
+
+import org.apache.alfred.storage.pub.AbstractFileSystem;
+import org.apache.alfred.storage.pub.AbstractPath;
+
+import org.apache.alfred.hf.pub.TimeInterval;
+
+import javax.annotation.Nullable;
+import java.util.Collection;
+
+/**
+ *
+ */
+public interface TSRoot {
+    public PersistentDataStore getStore();
+
+    public void open(boolean readOnly);
+
+    public boolean isOpen();
+
+    public void format();
+
+    public void delete();
+
+    public SymbolRegistry getSymbolRegistry();
+
+    public void setMaxFolderSize(int numTimeSlices);
+
+    public int getMaxFolderSize();
+
+    public void setMaxFileSize(int numBytes);
+
+    public int getMaxFileSize();
+
+    public String getCompression();
+
+    public void setCompression(String compression);
+
+    public void getTimeRange(int id, TimeRange out);
+
+    public void getTimeRange(TimeRange out);
+
+    public AbstractFileSystem getFileSystem();
+
+    public TimeInterval[] getTimeRanges(int[] ids);
+
+    /**
+     * Close this root gracefully. It must not be in active state.
+     */
+    public void close();
+
+    public AbstractPath getPath();
+
+    /**
+     * Force-close this root. Any unsaved data will be lost.
+     */
+    public void forceClose();
+
+    public void selectTimeSlices(TimeRange timeRange, EntityFilter filter, Collection<TSRef> addTo);
+
+    public String getPathString();
+
+    public void drop(TimeRange range);
+
+    public void iterate(TimeRange range, EntityFilter filter, TimeSliceIterator it);
+
+    public TSRef associate(String path);
+
+    int MAX_FILE_SIZE_DEF = 1 << 23;
+    int MAX_FILE_SIZE_LOW = 100;
+    int MAX_FILE_SIZE_HIGH = 100 << 20;
+
+    int MAX_FOLDER_SIZE_DEF = 100;
+    int MAX_FOLDER_SIZE_LOW = 10;
+    int MAX_FOLDER_SIZE_HIGH = 0xFFFF;
+
+    int COMPRESSION_LEVEL_LOCAL_FS = 0;
+    String COMPRESSION_DEF = System.getProperty("TimeBase.fileSystem.compression", "LZ4(5)");
+
+    /**
+     * @return name of space, may be {@code null} if space name was not explicitly
+     *         assigned
+     */
+    @Nullable
+    String getSpace();
+
+    /**
+     * Set name of the space for this root
+     */
+    void setSpace(String name);
+
+    /**
+     * Assigns index for this {@link TSRoot}. See {@link #getSpaceIndex()}
+     */
+    void setSpaceIndex(int index);
+
+    /**
+     * Index of this space for sorting operations.
+     *
+     * <pre>
+     *  signum(String.compare(r1.getSpace(), r2.getSpace()) == signum(Integer.compare(r1.getSpaceIndex(), r2.getSpaceIndex())
+     * </pre>
+     * 
+     * This means that sorting by value of getSpaceIndex() produces same order as
+     * sorting by {@link TSRoot#getSpace()}
+     */
+    int getSpaceIndex();
+}
